7bf995a389e51d41df6aa5c2ea6096c96f952b58
Ahmed Ashfaq <74646219+ahmedashfaq850@users.noreply.github.com>
2023-09-10 12:46:17 +0500

    add data type conversion

---
+++ 02_datatypes.js
@@ -0,0 +1,14 @@
+// datatypes in javascript
+// string => 'aa'
+// number => 23
+// boolean => true/false
+// null => stand alone value 
+// undefined => value is not assigned 
+
+
+
+let name = 'ahmed'
+let age = 23
+let isLoggedin = false
+console.log(typeof null) // return object
+console.log(typeof undefined) // return undefined
---
+++ 03_datatypeconverion.js
@@ -0,0 +1,19 @@
+// Number Conversion
+let score = '33'
+let valueConversion = Number(score)
+//console.log(typeof valueConversion)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// Boolean Conversion
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// String Conversion
+let someNum = 22
+let stringNumber = String(someNum);
+console.log(typeof stringNumber)
