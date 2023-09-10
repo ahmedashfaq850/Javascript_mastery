47bb8f24dfb93c49a99351943dc8e8183f9bbc1b
Ahmed Ashfaq <74646219+ahmedashfaq850@users.noreply.github.com>
2023-09-10 15:33:27 +0500

    add datatypes  interveiw file

---
+++ 05_Interview.js
@@ -0,0 +1,10 @@
+// Primitive Data Types
+// String, Number, Boolean, null, undefined, Symbol
+
+
+// Non Primitive Data Types OR Reference 
+// Arrays, Objects, Functions
+
+
+// Javascript is a dynamic type
+
