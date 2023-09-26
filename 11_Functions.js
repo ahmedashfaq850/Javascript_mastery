c98fb31308965eabfa72a7f66b261834069dd924
Ahmed Ashfaq <74646219+ahmedashfaq850@users.noreply.github.com>
2023-09-26 14:10:35 +0500

    add Functions file

---
+++ 11_Functions.js
@@ -0,0 +1,15 @@
+function sayHello(){
+    console.log('my name is ahmed')
+}
+
+sayHello()
+
+
+
+
+function addTwoNumbers(num1, num2){ // Parameters or ...restOperator
+    return num1+num2
+}
+
+console.log(addTwoNumbers(1,2)) // Arguments
+
